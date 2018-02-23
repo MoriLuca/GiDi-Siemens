a87e541cc8300168113eb745896ebd7e269b488d
gidilucamori <luca.mori@gidiautomazione.it>
2018-02-23 14:32:47 +0100

    implementato controller ajax GetDb1

--- GiDiSiemens/Controllers/AjaxController.cs
+++ GiDiSiemens/Controllers/AjaxController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Luca;
 
 namespace GiDiSiemens.Controllers
 {
     public class AjaxController : Controller
     {
-        public IActionResult Index()
+        private readonly IViewRenderService _viewRenderService;
+
+        public JsonResult GetDb1()
         {
-            return View();
+            Luca.L_Siemens.ReadPlcAndComposeSiemensWorkFromSiemensDB();
+            var result = _viewRenderService.RenderToStringAsync("Service/Read", Repo.SiemensRepo.SiemensWork);
+            return Json(result.Result);
         }
     }
 }
