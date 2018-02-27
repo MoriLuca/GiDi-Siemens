438d34da8ab33462a07e0b410852383d7848d8d8
gidilucamori <luca.mori@gidiautomazione.it>
2018-02-27 10:03:42 +0100

    aggiorata view getcb1 con il foreach all'interno della lista Data

--- GiDiSiemens/Classes/Luca_Siemens/Model/SiemensMemory.cs
+++ GiDiSiemens/Classes/Luca_Siemens/Model/SiemensMemory.cs
@@ -20,7 +20,7 @@ namespace Luca.Siemens.Models
             this.Data.Add(new SiemensTag(Luca.Siemens.Data.TypeOfTag.SingleVariable, S7.Net.DataType.DataBlock, S7.Net.VarType.Int, typeof(UInt16), 1, 2, "UInt16"));
             this.Data.Add(new SiemensTag(Luca.Siemens.Data.TypeOfTag.SingleVariable, S7.Net.DataType.DataBlock, S7.Net.VarType.DInt, typeof(Int32), 1, 4, "Int32"));
             this.Data.Add(new SiemensTag(Luca.Siemens.Data.TypeOfTag.SingleVariable, S7.Net.DataType.DataBlock, S7.Net.VarType.DInt, typeof(UInt32), 1, 8, "UInt32"));
-            this.Data.Add(new SiemensTag(Luca.Siemens.Data.TypeOfTag.SingleVariable, S7.Net.DataType.DataBlock, S7.Net.VarType.Real, typeof(double), 1, 12, "double"));
+            this.Data.Add(new SiemensTag(Luca.Siemens.Data.TypeOfTag.SingleVariable, S7.Net.DataType.DataBlock, S7.Net.VarType.Real, typeof(double), 1, 12, "Double"));
             this.Data.Add(new SiemensTag(Luca.Siemens.Data.TypeOfTag.SingleVariable, S7.Net.DataType.DataBlock, S7.Net.VarType.Real, typeof(double), 1, 16, "Long double"));
             this.Data.Add(new SiemensTag(Luca.Siemens.Data.TypeOfTag.SingleVariable, S7.Net.DataType.DataBlock, S7.Net.VarType.String, typeof(string), 1, 24, "Stringa", 10));
         }
