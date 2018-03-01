b699251016e5f0bd6dd7eb5e64ce5bee82574805
gidilucamori <luca.mori@gidiautomazione.it>
2018-03-01 16:52:09 +0100

    Aggiunta app console

---
+++ GiDi_Siemens_WebServer/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(r;
+                Thread.Sleep(300);
+            }
+            
+        }
+    }
+}
